7e2c7abd0ed6d20fd31fbf32dc7a94fac82a45a2
lavanyacovance <108861855+lavanyacovance@users.noreply.github.com>
2026-02-20 12:35:06 +0530

    Add files via upload

---
+++ Syneos Health R Training - Assigning Values and Vectors Exercises.R
@@ -0,0 +1,136 @@
+########################################################
+# Syneos Health R Training - Assigning Values Exercises#
+########################################################
+
+# Exercise 1:
+# Create a variable 'e' and set it to Euler's Number (2.71828)
+
+
+
+
+# Exercise 2:
+# Create a new variable 'p' and set it to 10
+
+
+
+
+# Exercise 3:
+# Create a new variable 'z' and set it to 2
+
+
+
+
+# Exercise 4:
+# What is the sum of 'e', 'p', and 'z'?
+
+
+
+
+# Exercise 5:
+# Assign the sum of 'e', 'p' and 'z' to 'x'
+
+
+
+
+# Exercise 6:
+# Create variables weight and height. Then create a new variable BMI using weight and height.
+# Note: The BMI formula is weight/height^2
+
+
+
+########################################################
+# Syneos Health R Training - Vectors Exercises   #
+########################################################
+library(dplyr)
+
+# Exercise 7: 
+# Create a vector including 3 numbers, assign it to 'vec1'
+
+
+
+# Exercise 8:
+# Find the mean and sum of your given vector
+
+
+
+
+# Vec2 is assigned with named values
+vec2 <- c(A = 1, B=2, C=3, E=4)
+
+# Exercise 9:
+# Extract 1st element from vec2 and assign it to val1  
+
+
+
+
+# Exercise 10:
+# Extract number assigned to B from vec2 and assign it to val2 
+
+
+
+
+########################################################
+# Syneos Health R Training - Logical Tests Exercises   #
+########################################################
+
+# Exercise 11:
+# Run the vector (vect) below and find values that are greater than or equal to 3
+vect <- c(2, 3.5, 4, 1.2, 9, 7.4, 3, 9, 7.4)
+
+
+
+
+# Exercise 12:
+# Using the vector, vect, above, find values that are equal to 7.4
+
+
+
+
+# Exercise 13:
+# Run the vector, cities, below and find values that do not equal "Raleigh"
+cities <- c("Wilmington", "Miami", "Toronto", "Quebec", "New York", "Princeton", "Raleigh", "San Francisco")
+
+
+
+
+# Exercise 14:
+# Using the vector, cities, find the cities that come before "Quebec" alphabetically.
+
+
+
+
+##########################################################
+# Syneos Health R Training - Boolean Operators Exercises #
+##########################################################
+
+# Exercise 15:
+# Using the vector, cities, find the cities that equal "Miami" or "New York"
+
+
+
+
+# Exercise 16:
+# Using the vector, cities, find the cities in "Wilmington", "Toronto", "Princeton", "Raleigh"
+
+
+
+
+# Exercise 17:
+# Run the vector, visits, below and find visits between 2 and 15
+visits <- c(0, 2, 4, 8, 12, 15, 18, 21, 28, 32, 36, 40, 48, 52)
+
+
+
+
+# Exercise 18:
+# Using the vector, visits, find visits less than 18, or greater than or equal to 32
+
+
+
+
+# Exercise 19:
+# Using the R pre-defined vector, letters, find the letters that are in your name
+letters
+
+
+
---
+++ Syneos Health R Training - Chaining Functions Exercises.R
@@ -0,0 +1,31 @@
+###########################################################
+# Syneos Health R Training - Chaining Functions Exercises #
+###########################################################
+
+# Load packages
+library(dplyr)
+
+pi <- 3.14159265
+# Exercise 1:
+# Using the pipe operator %>%, round pi to 3 decimal places and then find the square root. (Answer: 1.772569)
+
+
+
+
+vector1 <- c(97.2, 38.5, 146.3, 67.8) 
+# Exercise 2:
+# Using the pipe operator %>%, do the following tasks in order: round vector1 to 0 decimal places, find the mean, 
+# find sin(), round to 1 decimal place. (Answer: -0.7)
+
+
+
+
+vector2 <- c(2,5,11,17)
+# Exercise 3:
+# Using the pipe operator %>%, do the following tasks in order: find the square root of the elements of vector2, 
+# round to 3 decimal places, find the mean.  (Answer: 2.7725)
+
+
+
+
+
